19b5a903a5a5a0206f1f0b0fdc083b352fdf1791
Akash Dabhane <akashdabhane10@gmail.com>
2024-08-11 00:09:00 +0530

    user model updated by adding encrypt password, isPasswordCorrect function, generate acccess and refreshtoken

--- src/model/user.model.js
+++ src/model/user.model.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
     firstName: {
@@ -23,12 +25,58 @@ const userSchema = new mongoose.Schema({
     phone: {
         type: Number,
         length: [10, "Give valid phone number."]
-    }, 
+    },
     wishlist: {
         type: [mongoose.Schema.Types.ObjectId],
         ref: 'bookdb'
+    }, 
+    refreshToken: {
+        type: String
     }
 }, { timestamps: true })
+
+
+// encryption to password
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+// compare password
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+// generate access token
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            username: this.username,
+            phone: this.phone
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+// generate refresh token
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 
 const userdb = mongoose.model('userdb', userSchema);
 
